5a9ed2c240409757c2039134190745c3e57292f9
hugo.clems <hugoc.pro@gmail.com>
2019-03-15 09:46:22 +0100

    Menu + Interactions + Titres des pages

--- app/src/main/java/hutoch/m2dl/screamingbird/SelecteurNiveauActivity.java
+++ app/src/main/java/hutoch/m2dl/screamingbird/SelecteurNiveauActivity.java
@@ -1,7 +1,9 @@
 package hutoch.m2dl.screamingbird;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class SelecteurNiveauActivity extends AppCompatActivity {
 
@@ -9,6 +11,15 @@ public class SelecteurNiveauActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_selecteur_niveau);
+    }
+
+    /**
+     * Affiche le jeu.
+     * @param v non utilisé
+     */
+    public void clicJouer(View v) {
+        Intent intent = new Intent(this, JeuActivity.class);
+        startActivity(intent);
     }
 
 }
